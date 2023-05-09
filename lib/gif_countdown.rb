f9a3df1d10e83a29c603aec43fc19c4874387dbb
Stefan Wienert <info@stefanwienert.de>
2023-05-09 10:23:00 +0200

    Fix: Wrong loading

--- lib/gif_countdown.rb
+++ lib/gif_countdown.rb
@@ -1,10 +1,10 @@
-require 'gif_countdown/generator'
 
 require 'i18n'
 I18n.load_path << File.expand_path('../config/locales/gif_countdown.de.yml', __dir__)
 I18n.load_path << File.expand_path('../config/locales/gif_countdown.en.yml', __dir__)
 
 module GifCountdown
+  autoload :Generator, 'gif_countdown/generator'
   def self.generate(**args)
     GifCountdown::Generator.new(**args).call
   end
